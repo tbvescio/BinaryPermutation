f5684e5682482636686b49aa7062a99fd4423fd5
tbvescio <43358562+tbvescio@users.noreply.github.com>
2019-11-29 22:09:58 -0300

    final

---
+++ README.md
@@ -0,0 +1,16 @@
+# BinaryPermutation
+
+- Script that calculate all the posible binary combinations  with n  decimals.
+
+
+### Example:
+- Result with 3 decimals:
+
+	-	['0', '0', '0']
+	['0', '0', '1']
+	['0', '1', '0']
+	['0', '1', '1']
+	['1', '0', '0']
+	['1', '0', '1']
+	['1', '1', '0']
+	['1', '1', '1']
--- binary.py
+++ binary.py
@@ -1,20 +1,25 @@
 
-entrada = int(input("Ingrese numero de vairables: "))
+entrada = int(input("Ingrese numero de variables: "))
+
+
 def binary(x):
     s = bin(x)
-
     return s[2:]
 
 
-vector = []
-numero = 0
-for i in range(2**entrada): #se repite la cantidad de combinaciones
-    numero = str(binary(i)) 
-    numero = list(numero)
-    for x in range(entrada-len(numero)): #en los espacios que sobran pone 0
-        numero.insert(0, '0')
-    
-    print(list(numero))
+def main():
+
+	vector = []
+	numero = 0
+	for i in range(2**entrada): #se repite la cantidad de combinaciones
+	    numero = str(binary(i)) 
+	    numero = list(numero)
+	    for x in range(entrada-len(numero)): #en los espacios que sobran pone 0
+	        numero.insert(0, '0')
+	    
+	    print(list(numero))
+
+main()
 
 
 
